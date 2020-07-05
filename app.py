93ccdab62f1374a5c33f364a73842d290406f8c6
y437li <li.mac.kaka@gmail.com>
2020-07-04 21:39:27 -0400

    update api

--- app.py
+++ app.py
@@ -32,16 +32,24 @@ def image_rec():
 @app.route('/classify', methods = ["GET", "POST"])
 def classify():
     # Variables
-    _key = '55449c9e8cd844f9a17562e042f8088d'  # Here, paste your primary key
+    _key = 'db67eaca8c4b42669146d4352d5d0f8e'  # Here, paste your primary key
     _maxNumRetries = 10
-    _url = 'https://imagerec.cognitiveservices.azure.com/vision/v3.0/tag'
+    _url = 'https://hhaggancvs.cognitiveservices.azure.com/customvision/v3.0/Prediction/7e067a88-2755-42ef-9302-b729c90001b5/classify/iterations/Iteration2/image?'
     # Read the image into a byte array
     # image_data = open(image_path, "rb").read()
 
     image_data = request.files["file"].read()
-    headers = {'Ocp-Apim-Subscription-Key': _key,
-               'Content-Type': 'application/octet-stream'}
-    params = {'visualFeatures': 'Categories,Description,Color'}
+    headers = {
+        # Request headers
+        'Content-Type': 'application/json',
+        'Prediction-key': _key,
+    }
+
+    params = {
+        # Request parameters
+        'application': 'classifyModel',
+    }
+
     response = requests.post(
         _url, headers=headers, params=params, data=image_data)
     response.raise_for_status()
@@ -50,13 +58,10 @@ def classify():
     # relevant caption for the image is obtained from the 'description' property.
     analysis = response.json()
     print(analysis)
-    # image_caption = analysis["description"]["captions"][0]["text"].capitalize()
-    # Display the image and overlay it with the caption.
     image = Image.open(BytesIO(image_data))
     fig = Figure(figsize=(5, 5))
     axis = fig.add_subplot(1, 1, 1)
     axis.axis('off')
-    # axis.set_title(image_caption, size="x-large", y=-0.1)
     axis.imshow(image)
 
     pngImage = io.BytesIO()
@@ -68,13 +73,13 @@ def classify():
 
     reuslt = []
     for i in range(0, 4):
-        tag = analysis['tags'][i]
+        tag = analysis['predictions'][i]
         temp = []
-        temp.append(tag['name'])
-        temp.append('{}%'.format(round(tag['confidence'] * 100)))
+        temp.append(tag['tagName'])
+        temp.append('{}%'.format(round(tag['probability'] * 100)))
         reuslt.append(temp)
+    ### [['fruit','93%'],['pineapple','92%']]
     return render_template("/image_rec.html", image=pngImageB64String, cats= reuslt)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
