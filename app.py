c544bfdb8c3cfb33d35501f0c455641964215d34
y437li <li.mac.kaka@gmail.com>
2020-07-09 20:53:58 -0400

    Update

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ import base64
 
 app = Flask(__name__)
 
-
-
-@app.route('/')
+"""FIRST RESOURCE: MAIN PAGE
+Requests user to select and upload an image,
+which gets stored in image_path under Capture.png
+"""
+@app.route('/')   
 def image_rec():
     image_path = './static/image/Capture.png'
     image_data = open(image_path, "rb").read()
@@ -29,18 +31,30 @@ def image_rec():
 
     return render_template("/image_rec.html",image =pngImageB64String)
 
-@app.route('/classify', methods = ["GET", "POST"])
+
+
+
+"""SECOND RESOURCE: CLASSIFICATIONS RESULTS PAGE 
+Function that uses Azure's Custom Vision resource to classify an image
+Output: The image that was uploaded and the top 4 results
+Methods
+    POST: client is providing an image to the server
+    GET: client obtains classification results from the server
+"""
+@app.route('/classify', methods = ["GET", "POST"]) 
 def classify():
-    # Variables
-    _key = 'db67eaca8c4b42669146d4352d5d0f8e'  # Here, paste your primary key
+    _key = 'db67eaca8c4b42669146d4352d5d0f8e'  #Azure's custom vision resource API
     _maxNumRetries = 10
+    #This endpoint will look at the second iteration of the trained/custom vision classifier - prediction
     _url = 'https://hhaggancvs.cognitiveservices.azure.com/customvision/v3.0/Prediction/7e067a88-2755-42ef-9302-b729c90001b5/classify/iterations/Iteration2/image?'
+    
     # Read the image into a byte array
     # image_data = open(image_path, "rb").read()
 
-    image_data = request.files["file"].read()
+    image_data = request.files["file"].read() #Flask object that reads a file (image) by a form
     headers = {
-        # Request headers
+        # Request headers: they represent meta-data associated with the API request and response
+        #Format: dictionary
         'Content-Type': 'application/json',
         'Prediction-key': _key,
     }
@@ -71,6 +85,7 @@ def classify():
     pngImageB64String = "data:image/png;base64,"
     pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
 
+    #Obtain the four top predictions in this format: [['fruit','93%'],['pineapple','92%']]
     reuslt = []
     for i in range(0, 4):
         tag = analysis['predictions'][i]
@@ -78,8 +93,8 @@ def classify():
         temp.append(tag['tagName'])
         temp.append('{}%'.format(round(tag['probability'] * 100)))
         reuslt.append(temp)
-    ### [['fruit','93%'],['pineapple','92%']]
     return render_template("/image_rec.html", image=pngImageB64String, cats= reuslt)
 
+#Make the server (IP) publicly available (deployment)
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
